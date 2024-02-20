197d82f5d8bb25a51284fdbb13e774eb0fffa585
KusumaNaiduB <kusumanaidu8@gmail.com>
2024-02-20 17:00:30 +0530

    first commit

---
+++ ClassLibrary1/ClassLibrary1/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary1
+{
+    public class Student
+    {    
+        public int SId { get; set; }
+        public string? SName { get; set; }
+        public string? Address { get; set; }
+    }
+
+}
+
---
+++ ClassLibrary1/TestProject/TeachTest.cs
@@ -0,0 +1,26 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [TestFixture]
+    public class TeachTest
+    {
+        [Test]
+        public void Test_CreateTeacher()
+        {
+            // Arrange
+            var teacher = new Teacher();
+            teacher.TeacherId = 201;
+            teacher.Name = "Mrs. Lalitha";
+
+            // Act & Assert
+            Assert.AreEqual(201, teacher.TeacherId);
+            Assert.AreEqual("Mrs. Lalitha", teacher.Name);
+        }
+    }
+}
